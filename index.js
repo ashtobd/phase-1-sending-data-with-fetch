8c796198195834a21095101c49fa842cd44ce403
Brian Ashton <ashtobd@gmail.com>
2023-07-01 10:06:12 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    let data = {
+        "name": name,
+        "email": email
+    }
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(data),
+      })
+      .then(function(response) {
+        return response.json()
+      })
+      .then((info) => {
+        document.querySelector('body').innerHTML = info.id
+      })
+      .catch(error => document.querySelector('body').innerHTML = error)
+      }
+  
